0d5d0a51d8a9e529744fb40c73dc773ba26d43db
yadavdesh <bhardwajdesh@gmail.com>
2018-09-04 00:12:40 +0200

    design improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :products
-  get 'simple_pages/shop'
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
   root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :orders, only: [:index, :show, :create, :destroy]
 end
