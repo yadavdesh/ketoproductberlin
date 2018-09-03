5ef69be997228f7b2fdb1ea4cc81b5ead6be6a3c
yadavdesh <bhardwajdesh@gmail.com>
2018-09-03 18:58:36 +0200

    design improvements

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
