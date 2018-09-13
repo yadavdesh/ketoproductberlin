d7752de7ea45b349451af4cc911a24a261ab0066
yadavdesh <bhardwajdesh@gmail.com>
2018-09-13 11:46:49 +0200

    4.3 coustom controller orders

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :orders, only: [:index, :show, :create, :destroy]
+  resources :orders, only: [:index, :show, :create, :destroy]
 end
