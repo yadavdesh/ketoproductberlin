8362962f09431d89dd722cdad893b1a7bb8844cf
yadavdesh <bhardwajdesh@gmail.com>
2018-09-18 21:48:42 +0200

    5.1 routes, redirect and links

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
   end
 
   def new
+
   end
 
   def create
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,7 +43,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to "/simple_pages/landing_page", notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
