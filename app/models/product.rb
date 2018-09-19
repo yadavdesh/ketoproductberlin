4db0d465b611b8f0e285b35f260dc001ad676e7f
yadavdesh <bhardwajdesh@gmail.com>
2018-09-19 23:15:17 +0200

    5.2 search form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,12 @@
 class Product < ApplicationRecord
   has_many :orders
-
+=begin
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
   end
+=end
 
   def self.psearch(search_term)
-    Product.where("name ILIKE ?", "%#{search_term}%")
+    Product.where("name ilike ?", "%#{search_term}%")
   end
 end
