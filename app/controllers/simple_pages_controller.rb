f02b67644de6c1e0ddad528277bd960a6648d3d4
yadavdesh <bhardwajdesh@gmail.com>
2018-09-21 11:31:03 +0200

    5.3 contact form and email

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -5,4 +5,15 @@ class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+      ActionMailer::Base.mail(From:@email,
+        to: 'yadavdesh@gmail.com',
+        subjeck: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
+
 end
