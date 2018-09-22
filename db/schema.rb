84bf4f1d7127344f61624e51dbfe4d33607c2d8c
yadavdesh <bhardwajdesh@gmail.com>
2018-09-22 13:23:55 +0200

    contact form troubleshooting

--- db/schema.rb
+++ db/schema.rb
@@ -56,16 +56,4 @@ ActiveRecord::Schema.define(version: 2018_09_22_005540) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  reate_table "views", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_views_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
-  end
-
 end
