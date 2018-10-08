35e4b9bded5f5c52ade56936e58e0e9d3d70af3f
yadavdesh <bhardwajdesh@gmail.com>
2018-10-09 00:07:10 +0200

    6.9 jquery

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  get 'simple_pages/404'
   post 'simple_pages/thank_you'
   root 'simple_pages#landing_page'
   post 'payments/create'
