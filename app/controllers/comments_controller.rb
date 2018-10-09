9affb1c977073fce2f59c592e94ed3b3f6da1a90
yadavdesh <bhardwajdesh@gmail.com>
2018-10-09 23:45:23 +0200

    6.10 ajax implementation on ratings

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,17 +9,17 @@ class CommentsController < ApplicationController
     if @comment.save
       format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
+        format.js
       else
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    end
     end
   end
 
 
 
   def destroy
-    byebug
     @comment = Comment.find(params[:id])
     product = @comment.product
     @comment.destroy
